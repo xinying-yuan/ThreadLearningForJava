0d3bc031f7e0e9bb3547d724a0f8ff94d38af980
xinying <xinying.yuan@foxmail.com>
2018-12-02 13:23:56 +0800

    some introduction to ThradLocal obj

--- src/Thread_Specific_Storage/Main.java
+++ src/Thread_Specific_Storage/Main.java
@@ -7,4 +7,9 @@ public class Main {
 		new ClientThread("bob").start();
 		new ClientThread("chris").start();
 	}
+	
+//	most important obj to implement Thread_Specific_Storage is using ThreadLocal
+//  ThreadLocal stores specific obj for thread using (key,value) 
+//  key = Thread.currentThread()
+//  if set() method is not called then get() method return null because there is no obj in the box
 }
