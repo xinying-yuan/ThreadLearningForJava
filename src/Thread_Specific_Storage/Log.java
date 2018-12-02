22cd1e69c9b5addd503d08513350f2420ae0a78e
xinying <xinying.yuan@foxmail.com>
2018-12-02 13:58:02 +0800

    some modification to Log .java ,no need to call Log.close() directly

--- src/Thread_Specific_Storage/Log.java
+++ src/Thread_Specific_Storage/Log.java
@@ -21,7 +21,32 @@ public class Log {
 			//create a new tslog and set
 			log=new TSlog(Thread.currentThread().getName()+"-log.txt");
 			manager.set(log);
+		    startWatcher(log);
 		}
 		return log;
+	}
+	
+	//no need for Log.close() method 
+	private static void startWatcher(final TSlog log) {
+		//close log in this method
+		
+		Thread obj=Thread.currentThread();
+		System.out.println("start watching "+obj.getName());
+		Thread watcher=new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					obj.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				log.close();
+				System.out.println("thread being watchered ends,log has been closed in watcher thread");
+			}
+		});
+		watcher.start();
 	}
 }
